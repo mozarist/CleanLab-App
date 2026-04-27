d8db6ccd63baaa1d04cbe40713e206b3b67fd667
mozarist <azzammozarist.xpro@gmail.com>
2026-04-27 11:28:38 +0700

    feat: enhance tab layout with new icons and styles; add profile and search screens

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,9 +2,9 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 
 import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { House, Compass, CircleUser, Search } from 'lucide-react-native';
+import { colors } from '@/constants/color';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -12,22 +12,45 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: colors.primary,
+        tabBarInactiveTintColor: colors.muted,
+        tabBarStyle: {
+          position: 'absolute',
+          backgroundColor: colors.card,
+          borderTopColor: colors.border,
+          borderRadius: 24,
+          height: 110,
+          paddingTop: 12,
+        },
         headerShown: false,
         tabBarButton: HapticTab,
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: '',
+          tabBarIcon: ({ color }) => <House size={28} color={color} />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: '',
+          tabBarIcon: ({ color }) => <Compass size={28} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: '',
+          tabBarIcon: ({ color }) => <Search size={28} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: '',
+          tabBarIcon: ({ color }) => <CircleUser size={28} color={color} />,
         }}
       />
     </Tabs>
